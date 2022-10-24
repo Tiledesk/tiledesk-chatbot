0b11c81bbdf84ef6ff775a730b300115685b943b
sponzillo <andrea.sponziello@gmail.com>
2022-10-24 11:13:12 +0000

    test2

--- tybotRoute/index.js
+++ tybotRoute/index.js
@@ -15,6 +15,7 @@ const { TdCache } = require('./TdCache.js');
 const { TiledeskChatbot } = require('./models/TiledeskChatbot.js');
 
 
+
 //router.use(cors());
 router.use(bodyParser.json({limit: '50mb'}));
 router.use(bodyParser.urlencoded({ extended: true , limit: '50mb'}));
