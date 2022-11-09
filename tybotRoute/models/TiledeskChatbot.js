4b2e636f67b46bbf787ec56e84f817775e3b93de
Andrea Sponziello <andrea.sponziello@gmail.com>
2022-11-09 12:18:49 +0100

    more logs fixed

--- tybotRoute/models/TiledeskChatbot.js
+++ tybotRoute/models/TiledeskChatbot.js
@@ -249,7 +249,7 @@ class TiledeskChatbot {
         //});
         //return;
       }
-      console.log("form_reply is", form_reply)
+      //console.log("form_reply is", form_reply)
     }
   
     // FORM END
