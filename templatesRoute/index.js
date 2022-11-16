35f1e808e479bfccd242ecb902fa84d2a39f8964
sponzillo <andrea.sponziello@gmail.com>
2022-11-16 17:43:02 +0000

    more templates

--- templatesRoute/index.js
+++ templatesRoute/index.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
+cont templates = require("templates.json");
 
 router.get('/', (req, res) => {
   res.send('Hello Chatbot Templates!');
 });
 
 router.get('/templates', (req, res) => {
-  console.log("REQUEST BODY:", JSON.stringify(req.body));
-  const text = req.body.text;
-  console.log("query text:", text);
-  const botId = req.body.botId;
-  console.log("query botId:", botId);
-  res.send(
-    [
-      {
-        "template1": "blank"
-      },
-      {
-        "template2": "handoff"
-      }
-    ]
-  );
+  res.send(templates);
 });
-
