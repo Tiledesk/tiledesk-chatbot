c8bc4d24626e1efea5bda98713d9eefb74e6b0d4
sponzillo <andrea.sponziello@gmail.com>
2022-11-24 20:20:23 +0000

    forbidden 403 for not oublic chatbots

--- templatesRoute/index.js
+++ templatesRoute/index.js
@@ -3,50 +3,24 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 let Faq_kb = require('../tybotRoute/models/faq_kb');
 let faqService = require('../tybotRoute/models/faqService');
+let templates = require('./templates.json');
 
 router.get('/', (req, res) => {
   res.send('Hello Chatbot Templates!');
 });
 
 router.get('/public/templates', (req, res) => {
-  console.log("REQUEST BODY:", JSON.stringify(req.body));
-  const text = req.body.text;
-  console.log("query text:", text);
-  const botId = req.body.botId;
-  console.log("query botId:", botId);
-  res.send(
-    [
-      {
-        "name": "blank",
-        botId: "635b982a448a230012464b13",
-        description: "Start design your chatbot from a blank template",
-        icon: "https://firebasestorage.googleapis.com/v0/b/tiledesk-prod-v2.appspot.com/o/profiles%2F635b982a448a230012464b13%2Fphoto.jpg?alt=media&1669186993792"
-      },
-      {
-        "name": "handoff",
-        botId: "635b982a448a230012464b13",
-        description: "The chatbot greets and supports, switch to a human agent when needed",
-        icon: "https://firebasestorage.googleapis.com/v0/b/tiledesk-prod-v2.appspot.com/o/profiles%2F635b982a448a230012464b13%2Fphoto.jpg?alt=media&1669186993792"
-      },
-      {
-        name: "Quick replies",
-        botId: "635b982a448a230012464b13",
-        description: "Dynamic quick replies chatbot",
-        icon: "https://firebasestorage.googleapis.com/v0/b/tiledesk-prod-v2.appspot.com/o/profiles%2F635b982a448a230012464b13%2Fphoto.jpg?alt=media&1669186993792"
-      }
-    ]
-  );
+  res.send(templates);
 });
 
 router.get('/public/templates/:botid', (req, res) => {
   let id_faq_kb = req.params.botid;
   Faq_kb.findById(id_faq_kb, async (err, faq_kb) => {
+    console.log('FAQ-KB: ', faq_kb);
     if (err) {
       console.error('GET FAQ-KB ERROR ', err)
       return res.status(500).send({ success: false, msg: 'Error getting bot.' });
-    } else {
-      console.log('FAQ-KB: ', faq_kb)
-
+    } else if (faq_kb.public) {
       let faqs = null;
       try {
         faqs = await faqService.getAll(id_faq_kb); //.then((faqs) => {
@@ -65,6 +39,9 @@ router.get('/public/templates/:botid', (req, res) => {
         console.error('GET FAQ ERROR: ', err)
         return res.status(500).send({ success: false, msg: 'Error getting faqs.' });
       }
+    }
+    else {
+      res.status(403).send({success: false, message: "Forbidden"});
     }
   })
 });
