2d79cd95e40ef70d1b5b3127e1594e9e3b86642f
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-01-03 19:00:38 +0100

    fix

--- tybotRoute/index.js
+++ tybotRoute/index.js
@@ -154,7 +154,8 @@ router.post('/ext/:botid', async (req, res) => {
   // reply.actions = getMockActionsWithAgent();
   // reply.actions = getMockActionsWithAgentWhenOnline();
   // reply.actions = MockActions.ChangeDepartment();
-  // reply.actions = MockActions.Intent();
+  // reply.actions = MockActions.Intent(); // generates a loop!
+  console.log("reply:", reply);
   console.log("reply.actions:", reply.actions);
   if (reply.actions) { // structured actions (coming from chatbot designer)
     let directives = actionsToDirectives(reply.actions);
