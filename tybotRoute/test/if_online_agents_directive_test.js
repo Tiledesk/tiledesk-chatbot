7ab50dfdba6dd836486c69e89dc7b11b52bf738e
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-01-18 11:57:57 +0100

    fix

--- tybotRoute/test/if_online_agents_directive_test.js
+++ tybotRoute/test/if_online_agents_directive_test.js
@@ -13,35 +13,29 @@ describe('Directive DirIfOnlineAgents', function() {
         callback(null, [{}]); // one agent
       }
     };
-    close_directive = {
-      name: "close"
-    };
-    let dir = new DirClose({
-        tdclient: new MockTdClient(),
-        requestId: "A-REQUEST-ID"
+    directive = {};
+    let dir = new DirIfOnlineAgents({
+        tdclient: new MockTdClient()
     });
-    dir.execute(close_directive, () => {
+    dir.execute(directive, () => {
     });
   });
 
-  it('test directive DirClose in pipeline', async () => {
-    const message_text = `\\_tdclose`;
-    const answer = {
-      text: message_text,
-      attributes: {
-          splits: true,
-          directives: true,
-          markbot: true
+  it('DirIfOnlineAgents true & false intents (online agents = 0)', async () => {
+    class MockTdClient {
+      openNow(callback) {
+          callback(null, {});
       }
-    }
-    let directivesPlug = new DirectivesChatbotPlug({supportRequest: supportRequest, TILEDESK_API_ENDPOINT: "APIURL", token: "token", log: false, HELP_CENTER_API_ENDPOINT: "HELP_CENTER_API_ENDPOINT"});
-    const bot_answer = await ExtUtil.execPipelineExt(supportRequest, answer, directivesPlug, null, false);
-    // console.log("bot_answer:", bot_answer)
-    // console.log("directivesPlug.directives", directivesPlug.directives);
-    assert(bot_answer == null);
-    assert(directivesPlug.directives != null);
-    assert(directivesPlug.directives.length == 1);
-    assert(directivesPlug.directives[0].name === "close");
+      getProjectAvailableAgents(callback) {
+        callback(null, [{}]); // one agent
+      }
+    };
+    directive = {};
+    let dir = new DirIfOnlineAgents({
+        tdclient: new MockTdClient()
+    });
+    dir.execute(directive, () => {
+    });
   });
     
 });
