521cf1e4ac1fc78cfbe5e29a49f043db8c24733b
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-02-05 12:40:10 +0100

    Added evaluate Static (JSON) expression to TiledeskExpression

--- tybotRoute/TiledeskExpression.js
+++ tybotRoute/TiledeskExpression.js
@@ -5,48 +5,74 @@ class TiledeskExpression {
     // check valid operators (only those in operators are allowed)
     // check valid variable names /^[_0-9a-zA-Z\.]$/
 
-    static operators = {
+    static OPERATORS = {
         "equalAsNumbers" : {
-            applyPattern: "Number($1) === Number($2)"
+            name: "equalAsNumbers",
+            applyPattern: "Number(#1) === Number(#2)"
         },
         "equalAsStrings" : {
-            applyPattern: "String($1) === String($2)"
+            name: "equalAsStrings",
+            applyPattern: "String(#1) === String(#2)"
         },
         "notEqualAsNumbers" : {
-            applyPattern: "Number($1) !== Number($2)"
+            name: "notEqualAsNumbers",
+            applyPattern: "Number(#1) !== Number(#2)"
         },
         "notEqualAsStrings" : {
-            applyPattern: "String($1) !== String($2)"
+            name: "notEqualAsStrings",
+            applyPattern: "String(#1) !== String(#2)"
         },
-        ">" : {
-            applyPattern: "Number($1) > Number($2)"
+        "greaterThan" : {
+            name: "greaterThan",
+            applyPattern: "Number(#1) > Number(#2)"
         },
-        ">=" : {
-            applyPattern: "Number($1) >= Number($2)"
+        "greaterThanOrEqual" : {
+            name: "greaterThanOrEqual",
+            applyPattern: "Number(#1) >= Number(#2)"
         },
-        "<" : {
-            applyPattern: "Number($1) < Number($2)"
+        "lessThan" : {
+            name: "lessThan",
+            applyPattern: "Number(#1) < Number(#2)"
         },
-        "&&": {
+        "lessThanOrEqual" : {
+            name: "lessThanOrEqual",
+            applyPattern: "Number(#1) <= Number(#2)"
+        },
+        "AND": {
+            name: "AND",
             applyPattern: " && "
         },
-        "||": {
+        "OR": {
+            name: "OR",
             applyPattern: " || "
         },
         "startsWith": {
-            applyPattern: "$1.startsWith($2)"
+            name: "startsWith",
+            applyPattern: "String(#1).startsWith(String(#2))"
+        },
+        "startsWithIgnoreCase": {
+            name: "startsWithIgnoreCase",
+            applyPattern: "String(#1).toLowerCase().startsWith(String(#2).toLowerCase())"
         },
         "contains": {
-            applyPattern: "?"
+            name: "contains",
+            applyPattern: "#1.includes(#2)"
+        },
+        "containsIgnoreCase": {
+            name: "containsIgnoreCase",
+            applyPattern: "#1.toLowerCase().includes(#2.toLowerCase())"
         },
         "endsWith": {
-            applyPattern: "$1.endsWith($2)"
+            name: "endsWith",
+            applyPattern: "#1.toLowerCase().endsWith(#2.toLowerCase())"
         },
         "isEmpty": {
-            applyPattern: "$1 === \"\""
+            name: "isEmpty",
+            applyPattern: "#1 === \"\""
         },
         "matches": {
-            applyPattern: "?"
+            name: "matches",
+            applyPattern: "#1.matches(/#2/)"
         }
     }
 
@@ -58,6 +84,12 @@ class TiledeskExpression {
         // console.log("With variables:", JSON.stringify(variables));
         const result = new TiledeskExpression().evaluate(expression, variables);
         // console.log("Expression result:", result);
+        return result;
+    }
+
+    // public
+    evaluateStaticExpression(expression) {
+        const result = new TiledeskExpression().evaluate(expression);
         return result;
     }
 
@@ -77,6 +109,8 @@ class TiledeskExpression {
     // }
 
     evaluate(expression, context) {
+        console.log("evaluating:", expression)
+        console.log("context:", context)
         let res;
         try {
             const vm = new VM({
@@ -92,8 +126,90 @@ class TiledeskExpression {
         return res;
     }
 
-    JSON2condition(json) {
-        return "";
+    static JSONConditionToExpression(condition, variables) {
+        console.log("condition:", condition);
+        console.log("condition.operand1:", condition.operand1);
+        console.log("condition.operand2:", condition.operand2);
+        const operator_name = condition.operator;
+        const operator = TiledeskExpression.OPERATORS[operator_name];
+        console.log("operator:", operator);
+        const applyPattern = operator.applyPattern;
+        console.log("applyPattern:", applyPattern);
+        const operand1_s = TiledeskExpression.stringValueOperand(condition.operand1, variables);
+        console.log("operand1_s:", operand1_s);
+        const operand2_s = TiledeskExpression.stringValueOperand(condition.operand2, variables);
+        console.log("operand2_s:", operand2_s);
+        const expression = 
+            applyPattern
+                .replace("#1", operand1_s)
+                .replace("#2", operand2_s);
+        return expression;
+    }
+
+    static JSONGroupToExpression(group, variables) {
+        let conditions = group.conditions;
+        let group_expression = "";
+        console.log("conditions:", conditions)
+        for(let i = 0; i < conditions.length; i++) {
+            let part = conditions[i];
+            if (part.type === "condition") {
+                let expression = TiledeskExpression.JSONConditionToExpression(part, variables);
+                group_expression += expression;
+            }
+            else if (part.type === "operator") {
+                const operator = TiledeskExpression.OPERATORS[part.operatorName];
+                group_expression += operator.applyPattern;
+            }
+        }
+        return "(" + group_expression + ")";
+    }
+
+    static JSONGroupsToExpression(groups, variables) {
+        let full_expression = "";
+        console.log("groups:", groups)
+        for(let i = 0; i < groups.length; i++) {
+            let g = groups[i];
+            if (g.type === "expression") {
+                let group_expression = TiledeskExpression.JSONGroupToExpression(g, variables);
+                full_expression += group_expression;
+            }
+            else if (g.type === "operator") {
+                const operator = TiledeskExpression.OPERATORS[g.operatorName];
+                full_expression += operator.applyPattern;
+            }
+        }
+        return full_expression;
+    }
+
+
+    static stringValueOperand(operand, variables) {
+        // return operand;
+        if (!operand) {
+            return "\"\"";
+        }
+        else if (!variables) {
+            console.log("NO VARS")
+            return "\"" + operand + "\"";
+        }
+        else {
+            console.log("vars!", variables)
+            let _operand = operand.trim();
+            let operandAsString = "\"" + operand + "\"";
+            if (_operand.startsWith("$")) {
+                console.log("_operand with $", _operand);
+                let varName = _operand.replace(/\$/g, "");
+                console.log("varName:", varName);
+                let value = variables[varName];
+                if (value) {
+                    operandAsString = '"' + value + '"';
+                }
+                return operandAsString;
+            }
+            else {
+                return "\"" + operand + "\"";
+            }
+        }
+        
     }
 
 }
---
+++ tybotRoute/test/json_to_expression_test.js
@@ -0,0 +1,390 @@
+var assert = require('assert');
+const exp = require('constants');
+const { TiledeskExpression } = require('../TiledeskExpression');
+
+describe('JSON to expression', function() {
+  
+  // it('test condition operand lessThan (false)', async () => {
+  //   const condition = {
+  //     "type": "condition",
+  //     "operand1": "12",
+  //     "operator": TiledeskExpression.OPERATORS.lessThan.name,
+  //     "operand2": "10"
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONConditionToExpression(condition);
+  //   console.log("expression:", expression);
+  //   assert(expression === "Number(\"" + condition.operand1 + "\") < Number(\"" + condition.operand2 + "\")");
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === false);
+  // });
+
+  // it('test condition operand lessThan (true)', async () => {
+  //   const condition = {
+  //     "type": "condition",
+  //     "operand1": "10",
+  //     "operator": TiledeskExpression.OPERATORS.lessThan.name,
+  //     "operand2": "12"
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONConditionToExpression(condition);
+  //   console.log("expression:", expression);
+  //   assert(expression === "Number(\"" + condition.operand1 + "\") < Number(\"" + condition.operand2 + "\")");
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test condition operand greaterThan (true)', async () => {
+  //   const condition = {
+  //     "type": "condition",
+  //     "operand1": "12",
+  //     "operator": TiledeskExpression.OPERATORS.greaterThan.name,
+  //     "operand2": "10"
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONConditionToExpression(condition);
+  //   console.log("expression:", expression);
+  //   assert(expression === "Number(\"" + condition.operand1 + "\") > Number(\"" + condition.operand2 + "\")");
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test condition operand startsWith (true)', async () => {
+  //   const condition = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.startsWith.name,
+  //     "operand2": "And"
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONConditionToExpression(condition);
+  //   console.log("expression:", expression);
+  //   assert(expression === "String(\"" + condition.operand1 + "\").startsWith(String(\"" + condition.operand2 + "\"))");
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test condition group in "and" (true)', async () => {
+  //   const part1 = {
+  //     "type": "condition",
+  //     "operand1": "2",
+  //     "operator": TiledeskExpression.OPERATORS.greaterThan.name,
+  //     "operand2": "1"
+  //   }
+
+  //   const part2 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.AND.name
+  //   }
+
+  //   const part3 = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.startsWith.name,
+  //     "operand2": "And"
+  //   }
+
+  //   const group = {
+  //     type: "expression",
+  //     conditions: [ part1, part2, part3 ]
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONGroupToExpression(group);
+  //   console.log("expression:", expression);
+  //   assert(expression === "(" + "Number(\"2\") > Number(\"1\") && String(\"Andrea\").startsWith(String(\"And\"))" + ")");
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test condition group in "and" (false)', async () => {
+  //   const part1 = {
+  //     "type": "condition",
+  //     "operand1": "1",
+  //     "operator": TiledeskExpression.OPERATORS.greaterThan.name,
+  //     "operand2": "2"
+  //   }
+
+  //   const part2 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.AND.name
+  //   }
+
+  //   const part3 = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.startsWith.name,
+  //     "operand2": "Ond"
+  //   }
+
+  //   const group = {
+  //     conditions: [ part1, part2, part3 ]
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONGroupToExpression(group);
+  //   console.log("expression:", expression);
+  //   assert(expression === "(" + "Number(\"1\") > Number(\"2\") && String(\"Andrea\").startsWith(String(\"Ond\"))" + ")");
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === false);
+  // });
+
+  // it('test multiple conditions group (true)', async () => {
+  //   const part1 = {
+  //     "type": "condition",
+  //     "operand1": "2",
+  //     "operator": TiledeskExpression.OPERATORS.greaterThan.name,
+  //     "operand2": "1"
+  //   }
+
+  //   const part2 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.AND.name
+  //   }
+
+  //   const part3 = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.startsWith.name,
+  //     "operand2": "And"
+  //   }
+
+  //   const part4 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.OR.name
+  //   }
+
+  //   const part5 = {
+  //     "type": "condition",
+  //     "operand1": "3",
+  //     "operator": TiledeskExpression.OPERATORS.equalAsNumbers.name,
+  //     "operand2": "03"
+  //   }
+
+  //   const part6 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.AND.name
+  //   }
+
+  //   const part7 = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.equalAsStrings.name,
+  //     "operand2": "Andrea"
+  //   }
+
+  //   const group = {
+  //     conditions: [ part1, part2, part3, part4, part5, part6, part7 ]
+  //   }
+    
+  //   const expression = TiledeskExpression.JSONGroupToExpression(group);
+  //   console.log("expression:", expression);
+  //   assert(expression === '(Number("2") > Number("1") && String("Andrea").startsWith(String("And")) || Number("3") === Number("03") && String("Andrea") === String("Andrea"))');
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test multiple groups (true)', async () => {
+  //   const part1 = {
+  //     "type": "condition",
+  //     "operand1": "2",
+  //     "operator": TiledeskExpression.OPERATORS.greaterThan.name,
+  //     "operand2": "1"
+  //   }
+
+  //   const part2 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.AND.name
+  //   }
+
+  //   const part3 = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.startsWith.name,
+  //     "operand2": "And"
+  //   }
+
+  //   const part5 = {
+  //     "type": "condition",
+  //     "operand1": "3",
+  //     "operator": TiledeskExpression.OPERATORS.equalAsNumbers.name,
+  //     "operand2": "03"
+  //   }
+
+  //   const part6 = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.AND.name
+  //   }
+
+  //   const part7 = {
+  //     "type": "condition",
+  //     "operand1": "Andrea",
+  //     "operator": TiledeskExpression.OPERATORS.equalAsStrings.name,
+  //     "operand2": "Andrea"
+  //   }
+
+  //   const group1 = {
+  //     type: "expression",
+  //     conditions: [ part1, part2, part3 ]
+  //   }
+
+  //   const group_operator = {
+  //     "type": "operator",
+  //     "operatorName": TiledeskExpression.OPERATORS.OR.name
+  //   }
+
+  //   const group2 = {
+  //     type: "expression",
+  //     conditions: [ part5, part6, part7 ]
+  //   }
+
+  //   const groups = [group1, group_operator, group2];
+    
+  //   const expression = TiledeskExpression.JSONGroupsToExpression(groups);
+  //   console.log("full expression:", expression);
+  //   assert(expression === '(Number("2") > Number("1") && String("Andrea").startsWith(String("And"))) || (Number("3") === Number("03") && String("Andrea") === String("Andrea"))');
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test multiple groups with one condition and one group (true)', async () => {
+  //   const part1 = {
+  //     "type": "condition",
+  //     "operand1": "2",
+  //     "operator": TiledeskExpression.OPERATORS.greaterThan.name,
+  //     "operand2": "1"
+  //   }
+
+  //   const group1 = {
+  //     type: "expression",
+  //     conditions: [ part1 ]
+  //   }
+
+  //   const groups = [group1];
+    
+  //   const expression = TiledeskExpression.JSONGroupsToExpression(groups);
+  //   console.log("full expression:", expression);
+  //   assert(expression === '(Number("2") > Number("1"))');
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === true);
+  // });
+
+  // it('test multiple groups with one condition and one group (false)', async () => {
+  //   const part1 = {
+  //     "type": "condition",
+  //     "operand1": "2",
+  //     "operator": TiledeskExpression.OPERATORS.lessThan.name,
+  //     "operand2": "1"
+  //   }
+
+  //   const group1 = {
+  //     type: "expression",
+  //     conditions: [ part1 ]
+  //   }
+
+  //   const groups = [group1];
+    
+  //   const expression = TiledeskExpression.JSONGroupsToExpression(groups);
+  //   console.log("full expression:", expression);
+  //   assert(expression === '(Number("2") < Number("1"))');
+  //   const result = new TiledeskExpression().evaluateStaticExpression(expression);
+  //   console.log("result:", result);
+  //   assert(result === false);
+  // });
+
+  it('test variables, all set', async () => {
+    const part1 = {
+      "type": "condition",
+      "operand1": "$a",
+      "operator": TiledeskExpression.OPERATORS.lessThan.name,
+      "operand2": "$b"
+    }
+
+    const group1 = {
+      type: "expression",
+      conditions: [ part1 ]
+    }
+
+    const variables = {
+      "a": "1",
+      "b": "2"
+    }
+
+    const groups = [group1];
+    
+    const expression = TiledeskExpression.JSONGroupsToExpression(groups, variables);
+    console.log("full expression:", expression);
+    assert(expression === '(Number("1") < Number("2"))');
+    const result = new TiledeskExpression().evaluateStaticExpression(expression);
+    console.log("result:", result);
+    assert(result === true);
+  });
+
+  it('test variables, some not set', async () => {
+    const part1 = {
+      "type": "condition",
+      "operand1": "$a",
+      "operator": TiledeskExpression.OPERATORS.lessThan.name,
+      "operand2": "$b"
+    }
+
+    const group1 = {
+      type: "expression",
+      conditions: [ part1 ]
+    }
+
+    const variables = {
+      // "a": "1",
+      "b": "2"
+    }
+
+    const groups = [group1];
+    
+    const expression = TiledeskExpression.JSONGroupsToExpression(groups, variables);
+    console.log("full expression:", expression);
+    assert(expression === '(Number("$a") < Number("2"))');
+    const result = new TiledeskExpression().evaluateStaticExpression(expression);
+    console.log("result:", result);
+    assert(result === false);
+  });
+
+  // it('test one', async () => {
+  //   const expression = {
+  //     groups: [
+  //       {
+  //         type: "expression",
+  //         conditions: [
+  //           {
+  //             type: "condition",
+  //             "operand1": "12",
+  //             "operator": TiledeskExpression.OPERATORS.lessThan,
+  //             "operand2": "10"
+  //           },
+  //           {
+  //             type: "operator",
+  //             operator: "OR"
+  //           },
+  //           {}
+  //         ]
+  //       },
+  //       {
+  //         type: "operator",
+  //         operator: "AND"
+  //       },
+  //       {}
+  //     ]
+
+  //   };
+  // });
+    
+});
+
+
+
