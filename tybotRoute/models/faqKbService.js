e634327b47137654fe837a7babd660024d96d87f
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-02-08 19:07:46 +0100

    fix templates

--- tybotRoute/models/faqKbService.js
+++ tybotRoute/models/faqKbService.js
@@ -2,18 +2,20 @@ var Faq_kb = require("./faq_kb");
 
 class FaqKbService {
 
-  async getAll(options) {
-    if (!options) {
-      options = {
+  async getAll(_query) {
+    let query = _query;
+    if (!query) {
+      query = {
         public: true,
-        certified: true
+        certified: true,
+        "trashed": { $in: [null, false] }
       }
     }
     var sortQuery = {};
     sortQuery["score"] = -1;
     console.log("(Service) GET ALL FAQ_KBs");
     return new Promise((resolve, reject) => {
-      let query = {public: options.public, certified: options.certified};
+      // let query = {public: options.public, certified: options.certified};
       Faq_kb.find(query).sort(sortQuery).lean().exec( (err, bots) => {
         if (err) {
           reject(err);
