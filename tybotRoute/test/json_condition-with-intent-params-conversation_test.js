ef989f98bae0468b202f4b0674555bfe258a1d8e
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-03-13 22:44:57 +0100

    Added JSONCondition with intent parameters test

---
+++ tybotRoute/test/json_condition-with-intent-params-conversation_test.js
@@ -0,0 +1,281 @@
+var assert = require('assert');
+let axios = require('axios');
+const tybot = require("../");
+const tybotRoute = tybot.router;
+var express = require('express');
+var app = express();
+app.use("/", tybotRoute);
+app.use((err, req, res, next) => {
+  console.error("General error", err);
+});
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const bots_data = require('./json_condition-with-intent-params_bot.js').bots_data;
+
+const PROJECT_ID = "projectID"; //process.env.TEST_ACTIONS_PROJECT_ID;
+const REQUEST_ID = "support-group-" + PROJECT_ID + "-" + uuidv4().replace(/-/g, "");
+const BOT_ID = "botID"; //process.env.TEST_ACTIONS_BOT_ID;
+const CHATBOT_TOKEN = "XXX"; //process.env.ACTIONS_CHATBOT_TOKEN;
+
+describe('Conversation for JSONCondition with intent params test', async () => {
+
+  let app_listener;
+
+  before(() => {
+    return new Promise(async (resolve, reject) => {
+      console.log("Starting tilebot server...");
+      tybot.startApp(
+        {
+          // MONGODB_URI: process.env.mongoUrl,
+          bots: bots_data,
+          API_ENDPOINT: process.env.API_ENDPOINT,
+          REDIS_HOST: process.env.REDIS_HOST,
+          REDIS_PORT: process.env.REDIS_PORT,
+          REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+          log: process.env.API_LOG
+        }, () => {
+          console.log("Tilebot route successfully started.");
+          var port = process.env.PORT || 10001;
+          app_listener = app.listen(port, () => {
+            console.log('Tilebot connector listening on port ', port);
+            resolve();
+          });
+        });
+    })
+  });
+
+  after(function (done) {
+    app_listener.close(() => {
+      // console.log('ACTIONS app_listener closed.');
+      done();
+    });
+  });
+
+  it('/condition with params{"star_type":"supernova"}', (done) => {
+    console.log('/condition with params{"star_type":"supernova"}');
+    // let message_id = uuidv4();
+    let listener;
+    let endpointServer = express();
+    endpointServer.use(bodyParser.json());
+    endpointServer.post('/:projectId/requests/:requestId/messages', function (req, res) {
+      console.log("...req.body:", JSON.stringify(req.body));
+      res.send({ success: true });
+      const message = req.body;
+      assert(message.attributes.commands !== null);
+      assert(message.attributes.commands.length === 2);
+      const command2 = message.attributes.commands[1];
+    
+      assert(command2.type === "message");
+      assert(command2.message.text === "My name is supernova and I'm 2B km large");
+      getChatbotParameters(REQUEST_ID, (err, params) => {
+        if (err) {
+          assert.ok(false);
+        }
+        else {
+          assert(params);
+        //   assert(params["last_message_id"] === message_id);
+          assert(params["project_id"] === PROJECT_ID);
+          assert(params["star_type"] === "supernova");
+          assert(params["my_name"] === "supernova");
+          assert(params["size"] === "2B");
+          listener.close(() => {
+            done();
+          });
+        }
+      });
+
+    });
+
+    listener = endpointServer.listen(10002, '0.0.0.0', () => {
+      // console.log('endpointServer started', listener.address());
+      let request = {
+        "payload": {
+        //   "_id": message_id,
+          "senderFullname": "guest#367e",
+          "type": "text",
+          "sender": "A-SENDER",
+          "recipient": REQUEST_ID,
+          "text": '/condition with params{"star_type":"supernova"}',
+          "id_project": PROJECT_ID,
+          "metadata": "",
+          "request": {
+            "request_id": REQUEST_ID
+          }
+        },
+        "token": CHATBOT_TOKEN
+      }
+      sendMessageToBot(request, BOT_ID, () => {
+        // console.log("Message sent:\n", request);
+      });
+    });
+  });
+
+  it('/condition with params{"star_type":"nebula"}', (done) => {
+    console.log('/condition with params{"star_type":"nebula"}');
+    // let message_id = uuidv4();
+    let listener;
+    let endpointServer = express();
+    endpointServer.use(bodyParser.json());
+    endpointServer.post('/:projectId/requests/:requestId/messages', function (req, res) {
+      console.log("...req.body:", JSON.stringify(req.body));
+      res.send({ success: true });
+      const message = req.body;
+      assert(message.attributes.commands !== null);
+      assert(message.attributes.commands.length === 2);
+      const command2 = message.attributes.commands[1];
+    
+      assert(command2.type === "message");
+      assert(command2.message.text === "My name is nebula and I'm 500k km large");
+      getChatbotParameters(REQUEST_ID, (err, params) => {
+        if (err) {
+          assert.ok(false);
+        }
+        else {
+          assert(params);
+        //   assert(params["last_message_id"] === message_id);
+          assert(params["project_id"] === PROJECT_ID);
+          assert(params["star_type"] === "supernova");
+          assert(params["my_name"] === "nebula");
+          assert(params["size"] === "500kk");
+          listener.close(() => {
+            done();
+          });
+        }
+      });
+
+    });
+
+    listener = endpointServer.listen(10002, '0.0.0.0', () => {
+      // console.log('endpointServer started', listener.address());
+      let request = {
+        "payload": {
+        //   "_id": message_id,
+          "senderFullname": "guest#367e",
+          "type": "text",
+          "sender": "A-SENDER",
+          "recipient": REQUEST_ID,
+          "text": '/condition with params{"star_type":"nebula"}',
+          "id_project": PROJECT_ID,
+          "metadata": "",
+          "request": {
+            "request_id": REQUEST_ID
+          }
+        },
+        "token": CHATBOT_TOKEN
+      }
+      sendMessageToBot(request, BOT_ID, () => {
+        // console.log("Message sent:\n", request);
+      });
+    });
+  });
+
+});
+
+/**
+ * A stub to send message to the "ext/botId" endpoint, hosted by tilebot on:
+ * /${TILEBOT_ROUTE}/ext/${botId}
+ *
+ * @param {Object} message. The message to send
+ * @param {string} botId. Tiledesk botId
+ * @param {string} token. User token
+ */
+function sendMessageToBot(message, botId, callback) {
+  // const jwt_token = this.fixToken(token);
+  const url = `${process.env.TYBOT_ENDPOINT}/ext/${botId}`;
+  // console.log("sendMessageToBot URL", url);
+  const HTTPREQUEST = {
+    url: url,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    json: message,
+    method: 'POST'
+  };
+  myrequest(
+    HTTPREQUEST,
+    function (err, resbody) {
+      if (err) {
+        if (callback) {
+          callback(err);
+        }
+      }
+      else {
+        if (callback) {
+          callback(null, resbody);
+        }
+      }
+    }, false
+  );
+}
+
+/**
+ * A stub to get the request parameters, hosted by tilebot on:
+ * /${TILEBOT_ROUTE}/ext/parameters/requests/${requestId}?all
+ *
+ * @param {string} requestId. Tiledesk chatbot/requestId parameters
+ */
+function getChatbotParameters(requestId, callback) {
+  // const jwt_token = this.fixToken(token);
+  const url = `${process.env.TYBOT_ENDPOINT}/ext/parameters/requests/${requestId}?all`;
+  const HTTPREQUEST = {
+    url: url,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'get'
+  };
+  myrequest(
+    HTTPREQUEST,
+    function (err, resbody) {
+      if (err) {
+        if (callback) {
+          callback(err);
+        }
+      }
+      else {
+        if (callback) {
+          callback(null, resbody);
+        }
+      }
+    }, false
+  );
+}
+
+function myrequest(options, callback, log) {
+  if (log) {
+    console.log("API URL:", options.url);
+    console.log("** Options:", JSON.stringify(options));
+  }
+  axios(
+    {
+      url: options.url,
+      method: options.method,
+      data: options.json,
+      params: options.params,
+      headers: options.headers
+    })
+    .then((res) => {
+      if (log) {
+        console.log("Response for url:", options.url);
+        console.log("Response headers:\n", JSON.stringify(res.headers));
+        //console.log("******** Response for url:", res);
+      }
+      if (res && res.status == 200 && res.data) {
+        if (callback) {
+          callback(null, res.data);
+        }
+      }
+      else {
+        if (callback) {
+          callback(TiledeskClient.getErr({ message: "Response status not 200" }, options, res), null, null);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
+      if (callback) {
+        callback(error, null, null);
+      }
+    });
+}
---
+++ tybotRoute/test/json_condition-with-intent-params_bot.js
@@ -0,0 +1,159 @@
+const bot = {
+	"webhook_enabled": false,
+	"language": "en",
+	"name": "Link Button",
+	"type": "tilebot",
+	"intents": [{
+		"webhook_enabled": false,
+		"enabled": true,
+		"actions": [{
+			"_tdActionType": "reply",
+			"text": "Start\r\n",
+			"attributes": {
+				"commands": [{
+					"type": "wait",
+					"time": 500
+				}, {
+					"type": "message",
+					"message": {
+						"type": "text",
+						"text": "Start",
+						"attributes": {
+							"attachment": {
+								"type": "template",
+								"buttons": [{
+									"value": "/condition with params{\"star_type\":\"supernova\"}",
+									"type": "text",
+									"target": "blank",
+									"link": "",
+									"action": "",
+									"show_echo": true
+								}, {
+									"value": "/condition with params{\"star_type\":\"nebula\"}",
+									"type": "text",
+									"target": "blank",
+									"link": "",
+									"action": "",
+									"show_echo": true
+								}]
+							}
+						}
+					}
+				}]
+			},
+			"_tdActionTitle": null
+		}],
+		"question": "\\start",
+		"intent_display_name": "start",
+		"language": "en",
+		"intent_id": "550a5f10-f161-43d1-a308-97576797d54d"
+	}, {
+		"webhook_enabled": false,
+		"enabled": true,
+		"actions": [{
+			"_tdActionType": "reply",
+			"text": "I didn't understand. Can you rephrase your question?",
+			"attributes": {
+				"commands": [{
+					"type": "message",
+					"message": {
+						"type": "text",
+						"text": "I didn't understand. Can you rephrase your question?"
+					}
+				}]
+			}
+		}],
+		"question": "defaultFallback",
+		"intent_display_name": "defaultFallback",
+		"language": "en",
+		"intent_id": "543f9b38-376a-447b-ba9c-dda4cae91cd9"
+	}, {
+		"webhook_enabled": false,
+		"enabled": true,
+		"actions": [{
+			"_tdActionTitle": "star_type === supernova",
+			"_tdActionType": "jsoncondition",
+			"groups": [{
+				"type": "expression",
+				"conditions": [{
+					"type": "condition",
+					"operand1": "star_type",
+					"operator": "equalAsStrings",
+					"operand2": {
+						"type": "const",
+						"value": "supernova",
+						"name": ""
+					}
+				}]
+			}],
+			"stopOnConditionMet": false,
+			"trueIntent": "#1ccbf281-ea62-4567-a481-c1cc48266168",
+			"falseIntent": "#1ccbf281-ea62-4567-a481-c1cc48266168",
+			"trueIntentAttributes": {
+				"my_name": "supernova",
+				"size": "2B"
+			},
+			"falseIntentAttributes": {
+				"my_name": "nebula",
+				"size": "500k"
+			}
+		}],
+		"language": "en",
+		"intent_display_name": "condition with params",
+		"intent_id": "e538b93d-47e7-4888-8f23-e10f878ac901"
+	}, {
+		"webhook_enabled": false,
+		"enabled": true,
+		"actions": [{
+			"_tdActionTitle": null,
+			"_tdActionType": "setattribute",
+			"expression": "?",
+			"assignTo": "star_type"
+		}],
+		"language": "en",
+		"intent_display_name": "variables declaration",
+		"intent_id": "f629c3ab-b317-486d-802f-e7e458a4c839"
+	}, {
+		"webhook_enabled": false,
+		"enabled": true,
+		"actions": [{
+			"_tdActionTitle": null,
+			"_tdActionType": "reply",
+			"attributes": {
+				"disableInputMessage": false,
+				"commands": [{
+					"type": "wait",
+					"time": 500
+				}, {
+					"type": "message",
+					"message": {
+						"type": "text",
+						"text": "My name is ${my_name} and I'm ${size} km large"
+					}
+				}]
+			},
+			"text": "My name is ${my_name} and I'm ${size} km large\r\n"
+		}],
+		"language": "en",
+		"intent_display_name": "result",
+		"intent_id": "1ccbf281-ea62-4567-a481-c1cc48266168"
+	}]
+}
+
+// normalize the bot structure for the static intent search
+let intents = bot.intents;
+delete bot.intents;
+// console.log ("bot still is", JSON.stringify(bot));
+// console.log ("bintents still are", intents[0]);
+intent_dict = {};
+for (let i = 0; i < intents.length; i++) {
+  intent_dict[intents[i].intent_display_name] = intents[i];
+  intent_dict["#" + intents[i].intent_id] = intents[i];
+}
+bot.intents = intent_dict;
+const bots_data = {
+  "bots": {}
+}
+bots_data.bots["botID"] = bot;
+
+module.exports = { bots_data: bots_data };
