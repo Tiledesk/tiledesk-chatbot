bd9a3da9a182dcadc23608c25df2b568bb30f528
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-03-16 19:49:38 +0100

    added log for /ext endpoint debug

--- tybotRoute/index.js
+++ tybotRoute/index.js
@@ -390,7 +390,11 @@ router.post('/ext/:projectId/requests/:requestId/messages', async (req, res) => 
       id_project: projectId
     }
   }
-  // if (log) {console.log("request....", JSON.stringify(request));}
+  if (log) {
+    console.log("/ext request....", JSON.stringify(request));
+    console.log("/ext APIURL....", APIURL);
+    console.log("/ext process.env.TYBOT_ENDPOINT....", process.env.TYBOT_ENDPOINT);
+  }
   let directivesPlug = new DirectivesChatbotPlug({supportRequest: request, TILEDESK_API_ENDPOINT: APIURL, TILEBOT_ENDPOINT:process.env.TYBOT_ENDPOINT, token: token, log: log, HELP_CENTER_API_ENDPOINT: process.env.HELP_CENTER_API_ENDPOINT, cache: tdcache});
   // PIPELINE-EXT
   // if (log) {console.log("answer to process:", JSON.stringify(answer));}
