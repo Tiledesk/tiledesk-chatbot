9f12fe9078c0946a611a6c4d3a5f0b1bc913845b
Giovanni Troisi <giovanni@tiledesk.com>
2023-10-10 12:21:35 +0200

    debug tiledesk-ai

--- tybotRoute/models/IntentsMachineFactory.js
+++ tybotRoute/models/IntentsMachineFactory.js
@@ -6,12 +6,14 @@ class IntentsMachineFactory {
     static getMachine(bot, botId, projectId, log) {
       let machine;
       if (bot.intentsEngine === "tiledesk-ai") {
+        console.log("bot.intentsEngine is tiledesk-ai");
         machine = new TiledeskIntentsMachine(
           {
             botId: botId
           });
       }
       else {
+        console.log("bot.intentsEngine is null");
         if (log) {console.log("Setting MongodbIntentsMachine with bot:", JSON.stringify(bot));}
         machine = new MongodbIntentsMachine({projectId: projectId, language: bot.language, log});
       }
