1d80028e903564eb277174379da1b9ef4a4eefd9
Giovanni Troisi <giovanni@tiledesk.com>
2025-01-02 17:08:03 +0100

    improved ReplaceBotV2 action

--- tybotRoute/tiledeskChatbotPlugs/directives/DirReplaceBotV2.js
+++ tybotRoute/tiledeskChatbotPlugs/directives/DirReplaceBotV2.js
@@ -2,6 +2,9 @@ const { TiledeskClient } = require('@tiledesk/tiledesk-client');
 const { TiledeskChatbot } = require('../../models/TiledeskChatbot');
 const { Filler } = require('../Filler');
 
+const axios = require("axios").default;
+let https = require("https");
+
 class DirReplaceBotV2 {
 
   constructor(context) {
@@ -52,30 +55,125 @@ class DirReplaceBotV2 {
     );
     const filler = new Filler();
     botName = filler.fill(botName, variables);
-    this.tdClient.replaceBotByName(this.requestId, botName, () => {
-      if (blockName) {
-        if (this.log) {console.log("Sending hidden /start message to bot in dept");}
-        const message = {
-          type: "text",
-          text: "/" + blockName,
-          attributes : {
-            subtype: "info"
+
+    const HTTPREQUEST = {
+      url: this.API_ENDPOINT + "/" + this.context.projectId + "/requests/" + this.requestId + "/replace",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT ' + this.context.token
+      },
+      json: { name: botName},
+      method: 'PUT'
+    }
+
+    this.#myrequest(
+      HTTPREQUEST, async (err, resbody) => {
+        if (err) {
+          console.log("DirReplaceBot error: ", err);
+          if (callback) {
+            callback();
+            return;
           }
         }
-        this.tdClient.sendSupportMessage(
-          this.requestId,
-          message, (err) => {
-            if (err) {
-              console.error("Error sending hidden message:", err.message);
+
+        if (this.log) { console.log("DirReplaceBot replace resbody: ", resbody) };
+        if (blockName) {
+          if (this.log) { console.log("Sending hidden /start message to bot in dept"); }
+          const message = {
+            type: "text",
+            text: "/" + blockName,
+            attributes: {
+              subtype: "info"
             }
-            if (this.log) {console.log("Hidden message sent.");}
-            callback();
-        });
+          }
+          this.tdClient.sendSupportMessage(
+            this.requestId,
+            message, (err) => {
+              if (err) {
+                console.error("Error sending hidden message:", err.message);
+              }
+              if (this.log) { console.log("Hidden message sent."); }
+              callback();
+            });
+        }
+        else {
+          callback();
+        }
       }
-      else {
-        callback();
-      }
-    });
+    )
+
+    // this.tdClient.replaceBotByName(this.requestId, botName, () => {
+    //   if (blockName) {
+    //     if (this.log) {console.log("Sending hidden /start message to bot in dept");}
+    //     const message = {
+    //       type: "text",
+    //       text: "/" + blockName,
+    //       attributes : {
+    //         subtype: "info"
+    //       }
+    //     }
+    //     this.tdClient.sendSupportMessage(
+    //       this.requestId,
+    //       message, (err) => {
+    //         if (err) {
+    //           console.error("Error sending hidden message:", err.message);
+    //         }
+    //         if (this.log) {console.log("Hidden message sent.");}
+    //         callback();
+    //     });
+    //   }
+    //   else {
+    //     callback();
+    //   }
+    // });
+  }
+
+  #myrequest(options, callback) {
+    if (this.log) {
+      console.log("API URL:", options.url);
+      console.log("** Options:", JSON.stringify(options));
+    }
+    let axios_options = {
+      url: options.url,
+      method: options.method,
+      params: options.params,
+      headers: options.headers
+    }
+    if (options.json !== null) {
+      axios_options.data = options.json
+    }
+    if (this.log) {
+      console.log("axios_options:", JSON.stringify(axios_options));
+    }
+    if (options.url.startsWith("https:")) {
+      const httpsAgent = new https.Agent({
+        rejectUnauthorized: false,
+      });
+      axios_options.httpsAgent = httpsAgent;
+    }
+    axios(axios_options)
+      .then((res) => {
+        if (this.log) {
+          console.log("Response for url:", options.url);
+          console.log("Response headers:\n", JSON.stringify(res.headers));
+        }
+        if (res && res.status == 200 && res.data) {
+          if (callback) {
+            callback(null, res.data);
+          }
+        }
+        else {
+          if (callback) {
+            callback(new Error("Response status is not 200"), null);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("(DirAskGPT) Axios error: ", JSON.stringify(error));
+        if (callback) {
+          callback(error, null);
+        }
+      });
   }
 }
 
