d4ae4deee0b4a3fda312d16610e69041a378a034
Giovanni Troisi <giovanni@tiledesk.com>
2025-05-16 15:13:34 +0200

    updated logs of DirFlowLog and DirAddKbContent

--- tybotRoute/tiledeskChatbotPlugs/directives/DirAddKbContent.js
+++ tybotRoute/tiledeskChatbotPlugs/directives/DirAddKbContent.js
@@ -24,10 +24,11 @@ class DirAddKbContent {
     this.requestId = this.context.requestId;
     this.projectId = this.context.projectId;
     this.token = this.context.token;
-    this.intentDir = new DirIntent(context);
     this.API_ENDPOINT = this.context.API_ENDPOINT;
     this.log = context.log;
-    this.logger = new Logger({ request_id: this.requestId, dev: this.context.supportRequest.draft });
+    
+    this.intentDir = new DirIntent(context);
+    this.logger = new Logger({ request_id: this.requestId, dev: this.context.supportRequest?.draft, intent_id: this.context.reply?.attributes?.intent_info?.intent_id });
   }
 
   execute(directive, callback) {
