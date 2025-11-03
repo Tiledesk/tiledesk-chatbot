e6e865e5517b82a8f679ee29feb5caca26cd90ba
Giovanni Troisi <giovanni@tiledesk.com>
2025-11-03 15:31:39 +0100

    update

--- tybotRoute/tiledeskChatbotPlugs/DirectivesChatbotPlug.js
+++ tybotRoute/tiledeskChatbotPlugs/DirectivesChatbotPlug.js
@@ -249,7 +249,7 @@ class DirectivesChatbotPlug {
     }
     else if (directive_name === Directives.INTENT) {
       new DirIntent(context).execute(directive, async (stop) => {
-        console.log("[Performance] DirIntent finish execution on ", Date.now() - t1, "[ms]")
+        console.log("[Performance] DirIntent finish execution on ", Date.now())
         if (stop) {
           winston.debug("(DirectivesChatbotPlug) DirIntent Stopping Actions on: ", directive);
           this.theend();
