5fee803c93f9bbe0332e1265ef7c69c81b52be88
Giovanni Troisi <giovanni@tiledesk.com>
2026-04-14 16:49:27 +0200

    fix: update sender retrieval in DirAskGPTV2 to use new payload structure
    - Changed the sender property assignment to access senderFullname from the updated question_payload structure in the context.reply.attributes.
    - This adjustment ensures accurate sender information is captured for unanswered questions.

--- tybotRoute/tiledeskChatbotPlugs/directives/DirAskGPTV2.js
+++ tybotRoute/tiledeskChatbotPlugs/directives/DirAskGPTV2.js
@@ -499,7 +499,7 @@ class DirAskGPTV2 {
               namespace: json.namespace,
               question: json.question,
               request_id: this.requestId,
-              sender: this.context?.message?.senderFullname
+              sender: this.context?.reply?.attributes?.intent_info?.question_payload?.senderFullname
             }
             llmService.addUnansweredQuestion(this.projectId, data, this.token).catch((err) => {
               winston.error("DirAskGPTV2 - Error adding unanswered question: ", {
