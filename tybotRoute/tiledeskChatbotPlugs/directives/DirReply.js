68910ebe9374e2bf139b712870cf07d8ef9839ec
Gabriele Panico <gabriele.panico@gmail.com>
2026-04-22 13:02:02 +0200

    fix: update audio filename generation in DirReply to use a fixed .mp3 extension
    - Changed the filename format for audio files to consistently use the .mp3 extension instead of deriving it from content type.
    - This adjustment ensures compatibility and standardization for audio file handling in the chatbot's response mechanism.

--- tybotRoute/tiledeskChatbotPlugs/directives/DirReply.js
+++ tybotRoute/tiledeskChatbotPlugs/directives/DirReply.js
@@ -112,7 +112,8 @@ class DirReply {
               command.message.metadata = {
                 type: voiceSpeech.contentType,
                 uid: Date.now().toString(36),
-                filename: `audio-${Date.now().toString(36)}.${voiceSpeech.contentType.split('/')[1]}`,
+                // filename: `audio-${Date.now().toString(36)}.${voiceSpeech.contentType.split('/')[1]}`,
+                filename: `audio-${Date.now().toString(36)}.mp3`,
                 src: `${this.API_URL}/${this.projectId}/llm/speech`,
                 voiceSettings: voiceSettings
               }
