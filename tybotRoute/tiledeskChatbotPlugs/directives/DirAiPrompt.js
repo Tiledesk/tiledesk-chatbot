e2439ff004d3ce5cb7242d27a76dc8afa174aa99
Gabriele Panico <gabriele.panico@gmail.com>
2026-05-18 10:48:20 +0200

    Enhance logging in DirAiPrompt by adding a console log statement for requestVariables, improving visibility for debugging and monitoring the flow of data during chatbot operations.

--- tybotRoute/tiledeskChatbotPlugs/directives/DirAiPrompt.js
+++ tybotRoute/tiledeskChatbotPlugs/directives/DirAiPrompt.js
@@ -241,6 +241,7 @@ class DirAiPrompt {
         return;
       }
       
+      console.log('requestVariables', requestVariables);
       let flowVariables = {
         'x-chatbotToken': requestVariables.chatbotToken,
         'x-project_id': requestVariables.project_id,
