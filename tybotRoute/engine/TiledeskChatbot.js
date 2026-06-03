5713b1fd4bb164f8df906c1e13cc04df88116511
Gabriele Panico <gabriele.panico@gmail.com>
2026-06-03 17:05:24 +0200

    refactor: update logging statements in currentLockedMpc method of TiledeskChatbot
    Revised console log statements in the currentLockedMpc method to use a specific key format for tdcache operations, enhancing clarity and consistency in debugging output.

--- tybotRoute/engine/TiledeskChatbot.js
+++ tybotRoute/engine/TiledeskChatbot.js
@@ -502,9 +502,9 @@ class TiledeskChatbot {
     console.log('(TiledeskChatbot) currentLockedMpc', requestId);
     console.log("TDCache client info:", this.tdcache.client);
 
-    console.log("EXISTS:", await this.tdcache.client.exists(key));
-    console.log("TYPE:", await this.tdcache.client.type(key));
-    console.log("GET:", await this.tdcache.client.get(key));
+    console.log("EXISTS:", await this.tdcache.client.exists("tilebot:requests:"  + requestId + ":mcp:locked"));
+    console.log("TYPE:", await this.tdcache.client.type("tilebot:requests:"  + requestId + ":mcp:locked"));
+    console.log("GET:", await this.tdcache.client.get("tilebot:requests:"  + requestId + ":mcp:locked"));
 
     if (this.tdcache) {
       console.log('(TiledeskChatbot) currentLockedMpc ---> key:', "tilebot:requests:"  + requestId + ":mcp:locked");
